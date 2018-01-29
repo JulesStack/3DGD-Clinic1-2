a3a1bcf22bc79c048546ec1a352629c35e1bd227
JulesStack <35738452+JulesStack@users.noreply.github.com>
2018-01-28 21:35:00 -0500

    Troubleshoot2
    Testing meta files

---
+++ First person shooter/First person shooter/Assets/BasicAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAI : MonoBehaviour {
+
+    public Transform target;
+    public Transform myTransform;
+
+  
+
+    // Update is called once per frame
+    void Update () {
+
+        myTransform.LookAt(target);
+        myTransform.Translate(Vector3.forward * 3 * Time.deltaTime);
+
+	}
+}
