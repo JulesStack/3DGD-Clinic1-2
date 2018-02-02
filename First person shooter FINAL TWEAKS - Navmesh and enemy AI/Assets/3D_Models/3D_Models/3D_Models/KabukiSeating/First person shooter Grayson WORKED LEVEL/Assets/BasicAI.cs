3e7b3fd921a6d3e648d92701536d491e5894f592
Jennifer Belfield <jcabelfield@gmail.com>
2018-02-02 18:13:03 -0500

    Final tweaks level - Navmesh and enemy AI

---
+++ First person shooter FINAL TWEAKS - Navmesh and enemy AI/Assets/3D_Models/3D_Models/3D_Models/KabukiSeating/First person shooter Grayson WORKED LEVEL/Assets/BasicAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicAI : MonoBehaviour {
+
+	public Transform target;
+	NavMeshAgent agent;
+
+	void Start()
+	{
+
+		agent = GetComponent<NavMeshAgent>();
+
+	}
+
+
+	void Update () {
+
+		agent.SetDestination(target.position);
+
+	}
+}
